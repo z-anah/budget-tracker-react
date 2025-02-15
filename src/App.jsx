21fd1128de986e9982e9d97a34fc96443317c2eb
z-anah <40556052+z-anah@users.noreply.github.com>
2025-02-15 21:42:23 +0700

    docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from './screens/Home';
 import Project from './screens/Project';
 import Statistics from './screens/Statistics';
@@ -8,17 +8,23 @@ import Signup from './screens/Signup';
 import Category from './screens/Category';
 import NameAccounts from './screens/NameAccounts';
 
-const App = () => (
-  <Routes>
-    <Route path="/budget-tracker-react" element={<Home />} />
-    <Route path="/budget-tracker-react/project/:projectId" element={<Project />} />
-    <Route path="/budget-tracker-react/project/:projectId/transaction/:transactionId" element={<Transaction />} />
-    <Route path="/budget-tracker-react/project/:projectId/statistics" element={<Statistics />} />
-    <Route path="/budget-tracker-react/auth/login" element={<Login />} />
-    <Route path="/budget-tracker-react/auth/signup" element={<Signup />} />
-    <Route path="/budget-tracker-react/categories" element={<Category />} />
-    <Route path="/budget-tracker-react/accounts" element={<NameAccounts />} />
-  </Routes>
-);
+const App = () => {
+  const basePath = '/budget-tracker-react';
+  
+  return (
+    <Routes>
+      <Route path={basePath} element={<Home />} />
+      <Route path={`${basePath}/auth/login`} element={<Login />} />
+      <Route path={`${basePath}/auth/signup`} element={<Signup />} />
+      <Route path={`${basePath}/project/:projectId`} element={<Project />} />
+      <Route path={`${basePath}/project/:projectId/transaction/:transactionId`} element={<Transaction />} />
+      <Route path={`${basePath}/project/:projectId/statistics`} element={<Statistics />} />
+      <Route path={`${basePath}/categories`} element={<Category />} />
+      <Route path={`${basePath}/accounts`} element={<NameAccounts />} />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Navigate to={basePath} />} />
+    </Routes>
+  );
+};
 
 export default App;
